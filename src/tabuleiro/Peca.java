426a5ba80cd89b05310cc7d5333d4b232176e016
Amandickz <amandickz@gmail.com>
2024-12-06 14:36:14 -0300

    Enviando os arquivos iniciais

---
+++ src/tabuleiro/Peca.java
@@ -0,0 +1,15 @@
+package tabuleiro;
+
+public class Peca {
+
+    protected Posicao posicao;
+    private Tabuleiro tabuleiro;
+
+    public Peca(Posicao posicao) {
+        this.posicao = posicao;
+    }
+
+    protected Tabuleiro getTabuleiro() {
+        return tabuleiro;
+    }
+}
