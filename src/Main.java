981e57efd726df203a68549abc2902cc0162a813
Amandickz <amandickz@gmail.com>
2024-12-26 19:40:41 -0300

    Alteração de cores no tabuleiro

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-import aplicacao.Interface;
+import aplicacao.UI;
 import chess.ChessMatch;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
 
         ChessMatch partida = new ChessMatch();
-        Interface.imprimirTabuleiro(partida.getPieces());
+        UI.printBoard(partida.getPieces());
 
     }
 }
