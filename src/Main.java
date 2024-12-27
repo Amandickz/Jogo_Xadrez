53bbb61f1cb9c7595e9a5da88544d3fd57dee2ca
Amandickz <amandickz@gmail.com>
2024-12-27 15:58:17 -0300

    Inserção da lógica e verificação do Xeque Mate

--- src/Main.java
+++ src/Main.java
@@ -19,7 +19,7 @@ public class Main {
         ChessMatch chessMatch = new ChessMatch();
         List<ChessPiece> captured = new ArrayList<>();
 
-        while (true){
+        while (!chessMatch.getCheckMate()){
             try {
                 UI.clearScreen();
                 //UI.printBoard(chessMatch.getPieces());
@@ -49,6 +49,10 @@ public class Main {
                 scanner.nextLine();
             }
         }
+
+        UI.clearScreen();
+        UI.printMatch(chessMatch, captured);
+        scanner.close();
 
     }
 }
