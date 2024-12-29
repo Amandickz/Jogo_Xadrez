c792537ee9b16a7034fb43c233ac4c71047eca6a
Amandickz <amandickz@gmail.com>
2024-12-29 18:06:09 -0300

    Finalização do projeto

--- src/Main.java
+++ src/Main.java
@@ -44,9 +44,14 @@ public class Main {
 
                 if(chessMatch.getPromoted() != null){
                     System.out.print("Enter piece for promotion (B/C/T/Q): ");
-                    String type = scanner.nextLine();
+                    String type = scanner.nextLine().toUpperCase();
+                    while (!type.equals("B") && !type.equals("C") && !type.equals("T") & !type.equals("Q")) {
+                        System.out.print("Invalid value! Enter piece for promotion (B/C/T/Q): ");
+                        type = scanner.nextLine().toUpperCase();
+                    }
                     chessMatch.replacePromotedPiece(type);
                 }
+
             } catch (ChessException e){
                 System.out.println(e.getMessage());
                 scanner.nextLine();
